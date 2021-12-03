fa3c0a5c8833ed92bdc15d991d26464b286bcb27
Daniel Tate <daniellawrencetate@gmail.com>
2021-12-02 22:56:42 -0500

    Refactor + part 2.2
    Refactor to use traits, closures, and references. Still not sure that
    this is the most "rusty" way to do this. But it works.

--- aoc_2021_02/src/main.rs
+++ aoc_2021_02/src/main.rs
@@ -1,10 +1,16 @@
 /// Advent of Code 2021 project 02
 /// Daniel T. 2021-12-02
-/// Part 2 Goal: Find then multiply final horizontal position
+/// Part 2.1 Goal: Find then multiply final horizontal position
 /// by the final depth. 
+/// 
+/// Part 2.2 Goal: instructions are different. forward command increases position linearly
+/// and increases depth by distance * aim. 
+/// up and down control "aim". 
+/// Only forward can increase depth.
 
 use std::env;
 use file_input;
+use std::str;
 
 fn main() {
     // Take one commandline argument the path to the file with the 
@@ -18,23 +24,66 @@ fn main() {
     let content: Vec<String> = file_input::file_contents_as_vec(filename)
         .expect("Failed to open input file");
 
+    let parser = | x: &str | ->(String, i32) {
+        let split_stuff: Vec<&str> = x.split(' ').collect();
+        let command = str::to_owned(split_stuff[0]);
+        let mut num: i32 = 0;
+        if split_stuff.len() >= 2 {
+            num = split_stuff[1].parse::<i32>().unwrap();
+        }
+        
+        return (command, num);
+    };
+
+    // reset for part 2.2
+    // Try a more rusty way
+    let parsed_content: Vec<(String,i32)> = content.iter().map(|x| 
+        parser(x)).collect();
+
     let mut position: i32 = 0;
     let mut depth: i32 = 0;
-    for line in content {
+    for command in &parsed_content {
         // Honestly this feels a little wasteful in assignments, but oh well
-        let command: Vec<&str> = line.split(' ').collect();
-        let change: i32 = command[1].parse::<i32>().expect("Failed to parse int command");
-        let command: &str = command[0];
-        match command {
-            "forward" => { position = position + change },
-            "down" => { depth = depth + change },
-            "up" => { depth = depth - change },
+        
+        match command.0.as_str() {
+            "forward" => { position = position + command.1 },
+            "down" => { depth = depth + command.1 },
+            "up" => { depth = depth - command.1 },
             _other => {}
         }
         
     }
-
     let position_vector: i32 = position * depth;
+
+
+
+    let mut current_pos: SubPosition = SubPosition {
+        depth: 0,
+        aim: 0,
+        position: 0,
+    };
+
+    for command in &parsed_content {
+        match command.0.as_str() {
+            "forward" => { 
+                current_pos.position = current_pos.position + command.1;
+                current_pos.depth = current_pos.depth + (current_pos.aim * command.1);
+            },
+            "down" => { current_pos.aim = current_pos.aim + command.1 },
+            "up" => { current_pos.aim = current_pos.aim - command.1 }
+            _other => {}
+        }
+    }
+
+    let final_pos_vector = current_pos.depth * current_pos.position;
+    
     println!("Complete");
-    println!("Position: {}, Depth: {}, Final vector: {}", position, depth, position_vector);
+    println!("2.1:: Position: {}, Depth: {}, Final vector: {}", position, depth, position_vector);
+    println!("2.2:: Position: {}, Depth: {}, Aim: {}, Final vector: {}", current_pos.position, current_pos.depth, current_pos.aim, final_pos_vector)
+}
+
+struct SubPosition {
+    depth: i32,
+    aim: i32,
+    position: i32
 }
